af275961213905905836695693817618567bac59
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-04 17:14:59 +0200

    snack12/while

--- snack12/script.js
+++ snack12/script.js
@@ -1,12 +1,16 @@
 var firstName = ['Valentina', 'Federica', 'Rosaria', 'Antonio', 'Gianmarco'];
 var surName = ['Valcarcell', 'Palma', 'Petrungaro', 'Rossi', 'Esposito'];
+var guestList = [];
 
 document.getElementById('list').innerHTML = 'Lista degli invitati alla grande festa:';
 
-for (var x = 1; x <= 3; x++) {
-    var htmlString = '<li>';
-    htmlString += firstName[Math.floor(Math.random() * firstName.length)];
-    htmlString += " " + surName[Math.floor(Math.random() * surName.length)];
-    htmlString += '</li>';
-    document.getElementById('list-name').innerHTML += htmlString;
+while (guestList.length < 3) {
+    var guestName = firstName[Math.floor(Math.random() * firstName.length)];
+    var guestSurname = surName[Math.floor(Math.random() * surName.length)];
+    var guest = guestName + " " + guestSurname;
+
+    if (!guestList.includes(guest)) {
+        document.getElementById('list-name').innerHTML += '<li>' + guest + '</li>';
+        guestList.push(guest);
+    }
 }
